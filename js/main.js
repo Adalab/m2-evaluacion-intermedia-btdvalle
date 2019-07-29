9faa1d9737210f29c0e87861b7856cfc468f46d2
btdvalle <btdvalle@gmail.com>
2019-07-29 16:21:05 +0200

    add guessNumber function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+const myRandomNumber = getRandomNumber(100);
+
+console.log(`Mi número aleatorio es ${myRandomNumber}`);
+
+const guessNumber = function(variableinput, variableoutput) {
+  const input = document.querySelector(variableinput);
+  const output = document.querySelector(variableoutput);
+  const btn = document.querySelector(".btn");
+
+  function compareNumbers(event) {
+    event.preventDefault();
+    const inputNumber = parseInt(input.value);
+    if (myRandomNumber === inputNumber) {
+      output.innerHTML = "¡HAS GANADO CAMPEONA!";
+    } else if (inputNumber < myRandomNumber) {
+      output.innerHTML = "demasiado bajo";
+    } else if (inputNumber > myRandomNumber) {
+      output.innerHTML = "demasiado alto";
+    }
+  }
+  btn.addEventListener("click", compareNumbers);
+};
+
+guessNumber(".input", ".content");
